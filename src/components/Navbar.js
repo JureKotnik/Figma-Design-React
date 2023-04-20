8309df02f7c1e32f607f782baef5cc1386b76790
JureKotnik <jure2002k@gmail.com>
2023-04-20 08:14:52 +0200

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react';
+import styled from "styled-components";
+import { ReactComponent as LogoIcon } from '../assets/logo.svg';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <Nav>
+      <Container>
+            <LogoIcon />
+            <Hamburger onClick={() => setIsOpen(!isOpen)}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </Hamburger>
+            <Menu isOpen={isOpen}>
+              <LinkWrapper>
+              <MenuLink href=''>Search</MenuLink>
+              <MenuLink href=''>Login</MenuLink>
+              <Button>Join Now</Button>
+              </LinkWrapper>
+            </Menu>
+      </Container>
+    </Nav>
   )
 }
 
 export default Navbar
+
+const Container = styled.div``;
+const Nav = styled.div``;
+const Menu = styled.div``;
+const LinkWrapper = styled.div``;
+const MenuLink = styled.a``;
+const Button = styled.button``;
+const Hamburger = styled.div``;
+
